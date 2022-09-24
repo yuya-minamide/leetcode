59bffc697c04f7bad87269fdb2110ca721911ebf
Yuya Minamide <dettu1003@gmail.com>
2022-09-23 20:19:19 -0700

    1480. Running sum of 1d array.

---
+++ 1480.running-sum-of-1d-array.js
@@ -0,0 +1,15 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/running-sum-of-1d-array/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var runningSum = function(nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1];
+    }
+    return nums;
+};
