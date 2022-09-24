91c77c01ff1188f62f1a4c421c160d25f997a375
Yuya Minamide <dettu1003@gmail.com>
2022-09-23 20:18:22 -0700

    solved 1470.Shuffle the array

---
+++ 1470.shuffle-the-array.js
@@ -0,0 +1,17 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/shuffle-the-array/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+ var shuffle = function(nums, n) {
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(nums[i],nums[i + n])
+    }
+    return arr;
+};
