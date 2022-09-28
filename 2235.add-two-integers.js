9c1d0f226bcf3fb87996260657e212b3bad80bc2
Yuya Minamide <dettu1003@gmail.com>
2022-09-27 23:05:02 -0700

    solved 2235. Add two integers

---
+++ 2235.add-two-integers.js
@@ -0,0 +1,15 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/add-two-integers/
+ */
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
+
+
