f0815c76588f361318f0bda08e6831f157aea9e8
Yuya Minamide <dettu1003@gmail.com>
2022-10-10 19:41:19 -0700

    solved 1450. Number of students doing homework at a given time

---
+++ 1450.number-of-students-doing-homework-at-a-given-time.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    
+    let NumberOfStudents = 0;
+    
+    for (i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            NumberOfStudents++
+        }
+    }
+    
+    return NumberOfStudents;
+};
