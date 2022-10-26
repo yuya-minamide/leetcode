33780c5317a75e87a180e3d201a138ae3c1029ed
Yuya Minamide <dettu1003@gmail.com>
2022-10-26 15:59:45 -0700

    solved 709. To lower case

---
+++ 709.to-lower-case.js
@@ -0,0 +1,12 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/to-lower-case/
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
