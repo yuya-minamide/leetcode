95408091f7235e7c87f2c97a37f7f03848b6d115
Yuya Minamide <dettu1003@gmail.com>
2022-12-07 06:39:19 -0800

    solved 1572. Matrix diagonal sum

---
+++ 1572.matrixDiagonalSum.js
@@ -0,0 +1,19 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/matrix-diagonal-sum/description/
+ */
+
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function (mat) {
+	let result = 0;
+	for (let i = 0; i < mat.length; i++) {
+		for (let j = 0; j < mat[0].length; j++) {
+			if (i === j || i + j === mat[0].length - 1) result += mat[i][j];
+		}
+	}
+
+	return result;
+};
