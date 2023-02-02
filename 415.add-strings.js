8ecdbbee6c0b20d8e96736e2207b36517de3a00a
Yuya Minamide <dettu1003@gmail.com>
2023-02-02 12:10:04 -0800

    415. Add strings

---
+++ 415.add-strings.js
@@ -0,0 +1,13 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/add-strings/
+ */
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+	return String(BigInt(num1) + BigInt(num2));
+};
