565072dd12f896e1ade89b244f88136588db2527
Yuya Minamide <dettu1003@gmail.com>
2023-03-08 14:35:12 -0800

    solved 179. Largest Number with typescript

---
+++ 179.largest-number.ts
@@ -0,0 +1,5 @@
+// function largestNumber(nums: number[]): string {
+//   const SortedNums = nums.sort((a, b) => Number(`${b}${a}`) - Number(`${a}${b}`)).join('');
+
+//   return SortedNums.startsWith('0') ? '0' : SortedNums;
+// };
