aafcd28811cc190edce33f0b6fcfbe9394447d33
Yuya Minamide <dettu1003@gmail.com>
2023-03-09 09:19:29 -0800

    solved 905. Sort Array By Parity with typescript

---
+++ 905.sort-array-by-parity.ts
@@ -0,0 +1,6 @@
+// function sortArrayByParity(nums: number[]): number[] {
+//   const EvenNums = nums.filter(num => num % 2 === 0);
+//   const OddNums = nums.filter(num => num % 2 === 1);
+
+//   return EvenNums.concat(OddNums);
+// };
