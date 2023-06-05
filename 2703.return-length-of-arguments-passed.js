3bfdc2a26b1aa079688250c73ce18ea889f443dc
Yuya Minamide <dettu1003@gmail.com>
2023-06-04 17:21:31 -0700

    solved 2703. Return Length of Arguments Passed

---
+++ 2703.return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * URL of this problem
+ * https://leetcode.com/problems/return-length-of-arguments-passed/
+ */
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+	return [...args].length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
